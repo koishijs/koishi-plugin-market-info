3d45f4cf816d411f01a514b621c05abf04852b18
Shigma <shigma10826@gmail.com>
2023-01-06 19:09:19 +0800

    feat: support config showDeletion

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Context, Dict, Logger, Schema, Time } from 'koishi'
-import type { MarketPackage, MarketResult } from '@koishijs/registry'
+import type { AnalyzedPackage, MarketResult } from '@koishijs/registry'
 
 const logger = new Logger('market')
 
@@ -22,23 +22,25 @@ export const Rule: Schema<Rule> = Schema.object({
 export interface Config {
   rules: Rule[]
   interval: number
+  showDeletion: boolean
 }
 
 export const Config: Schema<Config> = Schema.object({
   rules: Schema.array(Rule).description('推送规则。'),
   interval: Schema.number().default(Time.minute * 30).description('轮询间隔 (毫秒)。'),
+  showDeletion: Schema.boolean().default(false).description('是否显示删除的插件。'),
 })
 
 function makeDict(result: MarketResult) {
-  const dict: Dict<MarketPackage> = {}
+  const dict: Dict<AnalyzedPackage> = {}
   for (const object of result.objects) {
-    dict[object.name] = object
+    dict[object.shortname] = object
   }
   return dict
 }
 
 export function apply(ctx: Context, config: Config) {
-  ctx.i18n.define('zh', require('./locales/zh-CN.yml'))
+  ctx.i18n.define('zh', require('./locales/zh-CN'))
 
   const getMarket = async () => {
     const data = await ctx.http.get<MarketResult>('https://registry.koishi.chat/market.json')
@@ -55,12 +57,13 @@ export function apply(ctx: Context, config: Config) {
 
     ctx.setInterval(async () => {
       const current = await getMarket()
-      const diff = Object.keys(current).map((name) => {
+      const diff = Object.keys({ ...previous, ...current }).map((name) => {
         const version1 = previous[name]?.version
         const version2 = current[name]?.version
         if (version1 === version2) return
-        if (!version1) return '新增：' + name
-        return `更新：${name} (${version1} → ${version2})`
+        if (!version1) return `新增：${name}`
+        if (version2) return `更新：${name} (${version1} → ${version2})`
+        if (config.showDeletion) return `删除：${name}`
       }).filter(Boolean).sort()
       previous = current
       if (!diff.length) return
